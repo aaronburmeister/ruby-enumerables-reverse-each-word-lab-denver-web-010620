49e888ea130029b49c92d9350f9080707221c80c
Aaron Burmeister <f34r91@gmail.com>
2019-12-27 15:48:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,3 @@ def reverse_each_word(string)
   word_array = word_array.collect {|a| a.reverse}
   word_array.join(" ")
 end
-
-example = "Hello, World!"
-#puts example.split(" ").each {|a| puts a.reverse}
-puts reverse_each_word(example)
